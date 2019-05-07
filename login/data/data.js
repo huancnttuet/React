c13d5fec14f70bb5106e09a099ee4db64ebda437
hyperminer <huancnttuet@gmail.com>
2019-05-08 00:39:54 +0700

    day3

--- login/data/data.js
+++ login/data/data.js
@@ -15,7 +15,7 @@ sequelize
   });
 
 module.exports = {
-  queryUser: async function (emailSignUp, usernameSignUp) {
+  checkSignUp: async function (emailSignUp, usernameSignUp) {
     var checkSignUp = await sequelize.query(`SELECT COUNT(email) AS email FROM user WHERE email='${emailSignUp}' OR username='${usernameSignUp}'`).then(result => {
       if(result[0][0].email > 0){
         return false
@@ -25,13 +25,23 @@ module.exports = {
     return checkSignUp;
   },
   createUser: async function (emailSignUp, usernameSignUp, pwdSignUp) {
-    var createUser = await sequelize.query(`INSERT INTO user(email,pwd,username) VALUES('${emailSignUp}','${usernameSignUp}','${pwdSignUp}') `).then(result => {
+    var createUser = await sequelize.query(`INSERT INTO user(email,username,pwd) VALUES('${emailSignUp}','${usernameSignUp}','${pwdSignUp}') `).then(result => {
       console.log(result);
       if(result[0] === 0 && result[1] === 1)
         return true;
       return false
     })
     return createUser
+  },
+  checkSignIn: async function (usernameSignIn, pwdSignIn) {
+    var checkSignIn = await sequelize.query(`SELECT COUNT(username) AS username FROM user WHERE pwd='${pwdSignIn}' AND username='${usernameSignIn}'`).then(result => {
+      if(result[0][0].username == 1){
+        return true
+      }
+      return false
+    })
+    return checkSignIn;
   }
+
 
 }
--- login/src/TopPage.js
+++ login/src/TopPage.js
@@ -5,12 +5,13 @@ export default function TopPage() {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Demo</Navbar.Brand>
+        <Navbar.Brand href="http://localhost:3000">Demo</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="http://localhost:3000">Home</Nav.Link>
+            <Nav.Link href="http://localhost:3000/signin">Signin</Nav.Link>
+            <Nav.Link href="http://localhost:3000/signup">Signup</Nav.Link>
             <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
