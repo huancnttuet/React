6c385cb04e98dba0e68a06758b4011d98ff2afdc
huancnttuet <huancnttuet@gmail.com>
2019-05-22 17:46:57 +0700

    test

--- login/src/__tests__/orm.test.js
+++ login/src/__tests__/orm.test.js
@@ -5,7 +5,10 @@ test('test orm', async () => {
   expect(p).toMatchObject({id:15})
 })
 
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
+Enzyme.configure({ adapter: new Adapter() });
 import React from 'react';
 
 import { shallow } from 'enzyme';
